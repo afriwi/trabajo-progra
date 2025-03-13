c3ef39399812daa14a9d711b4a34462227fa2dbf
Afra Rodríguez <164109513+afriwi@users.noreply.github.com>
2025-03-12 22:46:07 -0300

    Create robot choripan.py

---
+++ robot choripan.py
@@ -0,0 +1,5 @@
+res=str(input('hola, quieres un choripan?: '))
+while res == 'si':
+    res=str(input('toma, un choripan para ti ¿quieres otro?: '))
+    if res== 'no':
+        print('okey, no hay más choripán :c')
